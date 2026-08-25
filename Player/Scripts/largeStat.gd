ac99d6abc88fb67abade481f11f2b229096b937a
STEELAWASHERE <techshiftdnd@gmail.com>
2026-08-25 18:44:28 -0400

    Mid mMultLS fix

--- Player/Scripts/largeStat.gd
+++ Player/Scripts/largeStat.gd
@@ -3,7 +3,7 @@ class_name largeStat
 
 var value_array:	PackedInt64Array= [] 
 var dec: 						int=0
-var val_amount:					int	= 100
+var val_amount:					int	= 10
 var val_index:					int	= 0
 var base:						int	= 1000000000000000000 #1000000000000000000 = 1 quintillion
 
@@ -25,13 +25,19 @@ func string_to_big(_input: String) -> largeStat:
 	return output
 
 func _init(_input: Variant)->void:
-	
 	value_array.resize(val_amount)
 	value_array.fill(0)
-	val_index=value_array.size()-1
+	val_index=value_array.size()-2
 	#region String variant
 	if(_input is String):
 		var input_len: int = (log(base)/log(10))+1
+		
+		if _input.contains("."):
+			var helper: String = _input + "0000"
+			helper =  _input.substr(_input.find("."),-1)  
+			if helper.length() > 4: #Might cause issues where small values don't increase properly
+				helper=helper.substr(1,4)
+			value_array[value_array.size()-1]= helper.to_int()
 		while(_input.length() > input_len ):
 			value_array[val_index]= _input.substr(_input.length()-(log(base)/log(10))).to_int()
 			_input=_input.substr(0, _input.length()-(log(base)/log(10)))
@@ -42,22 +48,45 @@ func _init(_input: Variant)->void:
 	
 	#region int variant
 	if(_input is int):
-		value_array[val_index]=_input
+		value_array[value_array.size()-2]=_input
 	#endregion
 	#region float variant
 	if _input is float:
-		value_array[val_index]=_input
-		var helper: String = str(_input)
-		helper = helper.substr(helper.find("."),-1)
-		dec = int(helper)
+		var helper: String = str(_input) + "0000"
+		helper = helper.substr(helper.find(".")+1,4)
+		value_array[val_amount-1] = int(helper)
+		value_array[value_array.size()-2]=_input
 	#endregion
 	
-	pass
+	
+
+func break_float(_input: Variant) -> int:
+	#if helper.length() > 4: #Might cause issues where small values don't increase properly
+		#helper=helper.substr(0,4)
+	return 1
 
 func prestige() -> int:
 	return(val_amount-(val_index+1))
+func int_size(_input:int) -> int:
+	return (log(_input)/log(10))+1
 
 func get_val() -> String:
+	var output: String =""
+	var i: int = 0
+	while (i<value_array.size()-1):
+		if value_array[i]!=0: 
+			output=output+str(value_array[i])
+		i+=1
+	if value_array[value_array.size()-1] != 0:
+		output=output+"."+str(value_array[value_array.size()-1])
+		
+	#for x in value_array:
+		#if x==0:
+			#continue
+		#output=output+str(x)
+	
+	return output
+func get_raw_val() -> String:
 	var output: String =""
 	for x in value_array:
 		if x==0:
@@ -65,36 +94,29 @@ func get_val() -> String:
 		output=output+str(x)
 	
 	return output
-
 func addLS(_input: Variant) -> largeStat:
-	
 	var sum:	largeStat	=largeStat.new(0)
 	var carry:	int			=0
-	var i:		int			=val_amount-1
+	var i:		int			=val_amount-2
 	var addend : PackedInt64Array
 	var _dec: int = 0
-	var hcarry: int =0
+	var hcarry: int = 0
 	#region largeStat
 	if (_input is largeStat):
 		addend =_input.value_array
 	#endregion
-	#region int
-	if (_input is int):
+	elif _input is String or _input is int or _input is float:
 		var helper: largeStat=largeStat.new(_input)
-		addend = helper.value_array
-	#endregion
-	if _input is String:
-		var helper: largeStat=largeStat.new(_input)
-		addend = helper.value_array
+		addend = helper.value_array 	
 		
-	carry=int(dec+_dec)
-	dec= (dec+dec)-carry 
+	carry=(value_array[value_array.size()-1] + addend[addend.size()-1])/10000
+	sum.value_array[val_amount-1]= (value_array[value_array.size()-1] + addend[addend.size()-1])%10000
 		
 	while (i>0): 
 		sum.value_array[i]=(carry+value_array[i]+addend[i])%base
 		carry= (value_array[i]+addend[i])/base
 		
-		if (value_array[i]==0 && addend[i]==0):
+		if (value_array[i]==0 and addend[i]==0):
 			break
 		i-=1
 	sum.val_index=i
@@ -108,13 +130,13 @@ func multLS(_input: Variant)->largeStat:
 	var _dec: 	int		=0
 	var product: largeStat=largeStat.new(0)
 	var carry: 	int		=0
-	var i				=val_amount-1
+	var i				=val_amount-2
 	var fhelper:	float	=0
 	#region type determinant
 	if		(_input is int):
-		multiplier[0]=_input
+		multiplier[val_amount-2]=_input
 	elif	(_input is float):
-		multiplier[0]=int(_input)
+		multiplier[val_amount-2]=int(_input)
 		_dec=_input - int(_input)		
 	elif	(_input is largeStat):
 		var helper : largeStat
@@ -130,7 +152,11 @@ func multLS(_input: Variant)->largeStat:
 		multiplier[0]=1
 	#endregion
 	
-	carry=int(_input)
+	#carry=int(_input)
+	if value_array[val_amount-1]!=0 or multiplier[val_amount-1]!=0:
+		var hint: int=(carry+value_array[val_amount-1]*multiplier[val_amount-1])
+		product.value_array[val_amount-1]= hint%100000
+		carry=(value_array[val_amount-1]*multiplier[val_amount-1])/100000
 	
 	while (i>0): 
 		product.value_array[i]=(carry+value_array[i]*multiplier[i])%base
@@ -138,24 +164,12 @@ func multLS(_input: Variant)->largeStat:
 		if (value_array[i]==0 && multiplier[i]==0):
 			break
 		i-=1
-	
-	if (dec != 0) and (_dec != 0):
-		# turns dec and _dec into a proper decimal based on their length using natural log conversion
-		# then multiplies them together.
-		product.dec+= dec*_dec
-	while (i>0): #fix
-		product.value_array[i]=(carry+value_array[i]*_dec)%base
-		carry=(value_array[i]*_dec)/base
-		if (value_array[i]==0):
-			break
-		i-=1
-	
-	
-	#if (dec != 0) and (_dec == 0):
-		
-	
 	product.val_index=i 	
-	
+	if product.value_array[val_amount-1] != 0:
+		var helper:	String 	= product.get_raw_val()
+		var dec_len:	int = clamp(int_size(value_array[val_amount-1])	,0,INF) + clamp(int_size(multiplier[val_amount-1])	,0,INF)
+		helper= helper.substr(0,helper.length()-dec_len) + "." + helper.substr(helper.length()-dec_len,-1)
+		product = largeStat.new(helper)
 	
 	return product
 	
--- main.gd
+++ main.gd
@@ -1,11 +1,10 @@
 extends Node2D
-var a: largeStat	
-var b: largeStat	
-var c: largeStat = largeStat.new(0)
+var a: largeStat= largeStat.new(1.8)	
+var b: largeStat= largeStat.new(2.3)	
+var c: largeStat= largeStat.new(0)
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	a = largeStat.new(3)
-	b = largeStat.new(2.5)
+	
 	c = a.multLS(b)
 	print(c.get_val())
 	print("Hello")
